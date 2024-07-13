1c2e51817a19abbca6ff3918d87706caa48b615a
Deekshith <deekshith3538@gmail.com>
2024-07-13 22:07:01 +0530

    Second Card

--- qtify/src/Components/Section/Section.jsx
+++ qtify/src/Components/Section/Section.jsx
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import { Grid, Container,Accordion, AccordionSummary ,AccordionDetails, Typography} from '@mui/material';
-
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import Cardsfunction from "../Cards/Cards";
 import styles from "./Section.module.css"
 
 const Sections = () => {
   const [albums, setAlbums] = useState([]);
+  const [topalbums,setTopalbums]=useState([]);
 
   useEffect(() => {
     const fetchAlbums = async () => {
@@ -18,8 +18,17 @@ const Sections = () => {
         console.error('Error fetching albums:', error);
       }
     };
+    const fetchtopalbums=async()=>{
+      try {
+        const response = await fetch('https://qtify-backend-labs.crio.do/albums/new');
+        const data = await response.json();
+        setTopalbums(data);
+      } catch (error) {
+        console.error('Error fetching albums:', error);
+      }
+    }
 
-    fetchAlbums();
+    fetchtopalbums();
   }, []);
 
   return (
@@ -54,7 +63,7 @@ const Sections = () => {
       <Typography variant="h5" className={styles.SectionTitle}>New Albums</Typography>
       <Typography variant="body2" className={styles.ShowAll}>Show all</Typography></div>
       <Grid container spacing={2}>
-        {albums.map((album, index) => (
+        {topalbums.map((album, index) => (
           <Grid item key={index} xs={12} sm={6} md={4} lg={2}>
             <Cardsfunction data={album}  />
           </Grid>
