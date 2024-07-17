8863b26819303a2b50ed5c43b5937f921eb59d3c
Deekshith <deekshith3538@gmail.com>
2024-07-17 13:16:47 +0530

    Carousel updated the state and text

--- qtify/src/Components/Section/Section.jsx
+++ qtify/src/Components/Section/Section.jsx
@@ -1,37 +1,49 @@
-import React, { useEffect, useState } from 'react';
-import { Grid, Container,Accordion, AccordionSummary ,AccordionDetails, Typography} from '@mui/material';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import React, { useEffect, useState } from "react";
+import {
+  Grid,
+  Container,
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+  Typography,
+} from "@mui/material";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import Cardsfunction from "../Cards/Cards";
-import styles from "./Section.module.css"
-import Carousel from '../Carousel/Carousel';
+import styles from "./Section.module.css";
+import Carousel from "../Carousel/Carousel";
 
-const Sections = ({data,title}) => {
-
-  const[isCarosel,setIsCarosel]=useState(false);
+const Sections = ({ data, title }) => {
+  const [isCarosel, setIsCarosel] = useState(false);
 
   const toggleCarousel = () => {
-    setIsCarosel(prevState => !prevState);
+    setIsCarosel((prevState) => !prevState);
   };
 
   return (
     <Container className={styles.Containerclass}>
-        <div className={styles.SectionTitle}>
-<div>{title}</div>
-{
-  isCarosel?<div onClick={toggleCarousel}>Collapse</div>:<div onClick={toggleCarousel}>Show All</div>
-}
-</div>
-{
-  isCarosel?<Carousel Album={data} renderComponent={(data)=>  <Cardsfunction data={data}  />}/>:<Grid container spacing={2}>
-  {data.map((album, index) => (
-    <Grid item key={index} xs={12} sm={6} md={4} lg={2}>
-      <Cardsfunction data={album}  />
-    </Grid>
-  ))}
-</Grid> 
-}
-
-        
+      <div className={styles.SectionTitle}>
+        <div>{title}</div>
+        {
+          
+            isCarosel?<div className={styles.toggle} onClick={toggleCarousel}>Show All</div>:
+            <div className={styles.toggle} onClick={toggleCarousel}>Collapse</div>
+         
+        }
+      </div>
+      {isCarosel ? (
+        <Carousel
+          Album={data}
+          renderComponent={(data) => <Cardsfunction data={data} />}
+        />
+      ) : (
+        <Grid container spacing={2}>
+          {data.map((album, index) => (
+            <Grid item key={index} xs={12} sm={6} md={4} lg={2}>
+              <Cardsfunction data={album} />
+            </Grid>
+          ))}
+        </Grid>
+      )}
     </Container>
   );
 };
