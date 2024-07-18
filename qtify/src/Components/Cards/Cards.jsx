9a3a29859800fda987e08ce85680a50296db2e1c
Deekshith <deekshith3538@gmail.com>
2024-07-18 13:55:18 +0530

    Added tooltip to card

--- qtify/src/Components/Cards/Cards.jsx
+++ qtify/src/Components/Cards/Cards.jsx
@@ -9,11 +9,12 @@ import {
   Box,
   Container,
 } from "@mui/material";
+
 import React, { useEffect, useState } from "react";
 import styles from "./Cards.module.css";
 
 const Cardsfunction = ({ data, type }) => {
-  // console.log(data, "API Data");
+   console.log(data, "API Data");
   // console.log(type, "type");
   // let chipdata=1;
   // if(type==="Songs"){
@@ -28,9 +29,48 @@ const Cardsfunction = ({ data, type }) => {
   // if(type==="Songs"){
   //   setChipData(0);
   // }
+  switch(type){
+    case "Album":{
+
+      return (
+        <Box className={styles.BoxClass}>
+    
+    <Tooltip title={`${data.songs.length} songs`} placement="top" arrow>
+     
+          <Card className={styles.Cardsstyle}>
+            <CardMedia
+              component="img"
+              height="140"
+              image={data.image}
+              className={styles.CardImage}
+            />
+            <CardContent>
+              {chipData ? (
+                <Chip
+                  label={`${data.follows} Follows`}
+                  className={styles.ChipClass}
+                />
+              ) : (
+                <Chip label={`${data.likes} Likes`} className={styles.ChipClass} />
+              )}
+            </CardContent>
+          </Card>
+          <Typography sx={{ color: "#ffffff" }} textAlign="left">
+            {data.title}
+          </Typography>
+          </Tooltip>
+        </Box>
+      );
+      
+    }
+    case "Songs":{
+      
 
   return (
     <Box className={styles.BoxClass}>
+
+
+ 
       <Card className={styles.Cardsstyle}>
         <CardMedia
           component="img"
@@ -52,8 +92,14 @@ const Cardsfunction = ({ data, type }) => {
       <Typography sx={{ color: "#ffffff" }} textAlign="left">
         {data.title}
       </Typography>
+  
     </Box>
   );
+    }
+  }
+
+
+
 };
 
 export default Cardsfunction;
